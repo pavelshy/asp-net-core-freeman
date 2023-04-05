abe7d10dbabd754736e26f9d2669c59adb0f47b4
pavelshy <pavelshymko@gmail.com>
2023-04-05 11:49:25 +0200

    add new project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Firstproject.Models;
+
+namespace Firstproject.Controllers;
+
+public class HomeController : Controller
+{
+    public string Index()
+    {
+        return "Hello world";
+    }
+}
