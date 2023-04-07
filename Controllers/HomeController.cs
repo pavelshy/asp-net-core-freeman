0b8df646a988c19e56a652171a49482c7b2cc688
pavelshy <pavelshymko@gmail.com>
2023-04-07 08:48:22 +0200

    add new project PartyInvites

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,11 +6,8 @@ namespace Firstproject.Controllers;
 
 public class HomeController : Controller
 {
-    public ViewResult Index()
+    public IActionResult Index()
     {
-        int hour = DateTime.Now.Hour;
-        string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
-        
-        return View("MyView", viewModel);
+        return View();
     }
 }
